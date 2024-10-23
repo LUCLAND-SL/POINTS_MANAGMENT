442aa847c1a51745a9102627499596705be4899b
LUCLAND-SL <lucas.gutierrez@lucland.com>
2024-10-23 22:45:06 +0200

    Se añade rectángulo e imagen

--- points-managment/src/app/logic/AppLogic.ts
+++ points-managment/src/app/logic/AppLogic.ts
@@ -13,13 +13,24 @@ export class AppLogic {
     // Crear una instancia de jsPDF
     const doc = new jsPDF();
 
-    // Añadir contenido al PDF
-    doc.text(`Nombre: ${Nombre}`, 10, 10);
-    doc.text(`Apellidos: ${Apellidos}`, 10, 20);
-    doc.text(`DNI: ${DNI}`, 10, 30);
-    doc.text(`Número de Puntos: ${Num_puntos}`, 10, 40);
+    doc.setFillColor(112, 173, 71); // Color #70ad47
+    doc.rect(10, 10, 190, 6, 'F'); // Rectángulo lleno
 
-    // Guardar el PDF
-    doc.save('datos_usuario.pdf');
+    // Añadir la imagen
+    const img = new Image();
+    img.src = 'assets/LUCLAND-NO-BAK-WHITE.png';
+
+    img.onload = () => {
+      doc.addImage(img, 'PNG', 10, 20, 60, 40);
+  
+      // Añadir contenido al PDF
+      doc.text(`Nombre: ${Nombre}`, 10, 100);
+      doc.text(`Apellidos: ${Apellidos}`, 10, 110);
+      doc.text(`DNI: ${DNI}`, 10, 120);
+      doc.text(`Número de Puntos: ${Num_puntos}`, 10, 130);
+  
+      // Guardar el PDF
+      doc.save('datos_usuario.pdf');
+    };
   }
 }
