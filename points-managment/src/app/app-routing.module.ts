88422268caf84d0ae08d303c8687993cd967a8de
LUCLAND-SL <lucas.gutierrez@lucland.com>
2024-11-03 22:27:10 +0100

    Prueba GitHub Pages

--- points-managment/src/app/app-routing.module.ts
+++ points-managment/src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
-import { RouterModule } from "@angular/router";
-import { routes } from "./app.routes";
+import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { HistoricoComponent } from "./components/historico/historico.component";
+import { NormasComponent } from "./components/normas/normas.component";
+import { EmpalmadoresComponent } from "./components/empalmadores/empalmadores.component";
+import { HistoricoEmpComponent } from "./components/historico-emp/historico-emp.component";
+
+export const routesDef: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'historico', component: HistoricoComponent},
+    { path: 'normas', component: NormasComponent},
+    { path: 'empalmador', component: EmpalmadoresComponent},
+    { path: 'historico-emp', component: HistoricoEmpComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routesDef)],
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
