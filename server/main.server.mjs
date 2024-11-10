167eefe4a8e1bc60b8cc733f31caea585b2c97b7
LUCLAND-SL <lucas.gutierrez@lucland.com>
2024-11-10 23:56:17 +0100

    Auto-generated commit

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-XXO7JEDC.mjs";import"./chunk-I7DCYWR3.mjs";import"./chunk-SYQXOUTE.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
